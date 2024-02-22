7d6c6a29708c7ba8b3e7d9d85a74e62da97399d4
jjstyles0001 <97764916+0ptiKali1usion@users.noreply.github.com>
2024-02-21 23:44:26 -0700

    Add files via upload

---
+++ newlist_of_downloads.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+## by: 0ptiKaLi1usioN, aka jjstyles0001
+##
+## according to my comparison of differences analysis (looking a contents of /System/Library/Receipts, and the get_gb script i made
+## these ALL did not get installed. 
+## I cannot account for the gaps in number sequence, other than, 1) i neglected to find all the plists? 2) Apple chose to scratch that pack.
+## Regardless, I need these. 60, out of the 69, that did not get installed by default, with my GarageBand v10.3.5 & LogicPro 10.4.1
+## or do I have Logic 10.2.something? whatever man. g'd l'k.
+## -JJ
+## PS/BTW, this is just to prove how useful this script I made is, you should catalog your own collection of packs, and ascertain what, 
+## that, as an Apple computer corporation customer, and license holder/agreeer, what exactly YOU are entitled to, and, exercise your privilege(s) accordingly.
+## cool? whatever. peace.
+## PPS: like I also needed MAContent10_AlchemyPatchesACPs_Atmospheric.pkg, MAContent10_AlchemyPatchesACPs_Basic.pkg, 
+## MAContent10_AlchemyPatchesACPs_BasicAdd.pkg, MAContent10_AlchemyPatchesACPs_Cinematic.pkg, MAContent10_AlchemyPatchesACPs_Dance.pkg,
+## MAContent10_AlchemyPatchesACPs_ModernSongwriter.pkg, MAContent10_AlchemyPatchesACPs_ModernSynth.pkg,
+## MAContent10_AlchemyPatchesACPs_TexturesNEffects.pkg, MAContent10_AlchemyPatchesACPs_VintageSynth.pkg
+## but, I already downloaded & installed those, so, they are not in my "example" script here.
+## shit man, I don't even get paid to code, fuck you and fuck this, and fuck crapple.... I think I need a pill/meds. bye.
+
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2015/MAContent10_AssetPack_0305_AlchemyVocalsVocalPhrasesAnastacia.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2015/MAContent10_AssetPack_0306_AlchemyVocalsChoirSingle.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2015/MAContent10_AssetPack_0307_AlchemyBasicAdd.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0310_UB_DrumMachineDesignerGB.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0311_UB_DrumMachineDesignerLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0312_UB_UltrabeatKitsGBLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0313_UB_UltrabeatKitsLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0516_DrummerBluebird.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0517_DrummerBrooklyn.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0518_DrummerDetroitGarage.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0519_DrummerEastBay.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0520_DrummerFourOnTheFloor.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0521_DrummerHeavy.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0522_DrummerLiverpool.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0523_DrummerManchester.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0524_DrummerMotownRevisited.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0525_DrummerNeoSoul.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0526_DrummerPortland.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0527_DrummerRetroRock.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0528_DrummerRoots.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0529_DrummerScientificMethod.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0530_DrummerSlowJam.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0531_DrummerSmash.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0532_DrummerSoCal.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0533_DrummerSunset.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0534_DrummerStudio.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0535_DrummerTightMaple.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0536_DrummerClapsCowbell.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0537_DrummerShaker.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0538_DrummerSticks.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0539_DrummerTambourine.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0540_PlugInSettingsGB.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0541_PlugInSettingsGBLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0542_PlugInSettingsLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0543_DemosongsLogic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0556_PlugInSettingsLogicAlchemyBasic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0557_IRsSharedAUX.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0558_GBLogicAlchemyEssentials.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0559_LogicAlchemyEssentials.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0599_GBLogicAlchemyEssentials.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0600_LogicAlchemyEssentials.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0606_AlchemySettingsBasicEssential.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0607_AlchemySettingsAtmospheric.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0608_AlchemySettingsBasic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0609_AlchemySettingsCinematic.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0610_AlchemySettingsDance.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0611_AlchemySettingsModernSongwriter.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0612_AlchemySettingsModernSynth.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0613_AlchemySettingsTexturesNEffects.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0614_AlchemySettingsVintageSynth.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0716_EXS_MiniMarimba.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0717_EXS_ToyGlockenspiel.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0718_EXS_MusicBox.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0719_EXS_ToyCelesta.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0720_EXS_ToyPiano.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0721_EXS_XyloKeyboard.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0722_EXS_ElectronicDrumkits70sRhythmBox.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0723_EXS_ToyMelodica.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0724_EXS_ElectronicDrumkitsKnickKnack.pkg'
+wget 'https://audiocontentdownload.apple.com/lp10_ms3_content_2016/MAContent10_AssetPack_0725_EXS_PercussionPotsAndPans.pkg'
